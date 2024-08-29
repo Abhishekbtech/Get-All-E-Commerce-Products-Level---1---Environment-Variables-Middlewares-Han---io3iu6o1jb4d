93ee769ad8b6e6eb78dcbe7c6e3dba2915ee04a2
Abhishek Chauhan <128348666+Abhishekbtech@users.noreply.github.com>
2024-08-29 13:59:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // Importing products from products.json file
-const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+const products = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'products.json'), 'utf8'));
 
 // Middlewares
 app.use(express.json());
 
-// Write GET endpoint for sending all the products to client here
-// Endpoint - /api/v1/products
+// GET endpoint for sending all the products to client
+// Endpoint - /products
+app.get('/api/v1/products', (req, res) => {
+    if (products && products.length) {
+        res.statusCode = 200;
+        const payload = {
+            status: "success",
+            message: "Product fetched successfully",
+            data: {
+                products,
+            },
+        };
+        res.json(payload);
+    } else {
+        res.statusCode = 404;
+        const payload = { message: "Product not found" };
+        res.json(payload);
+    }
+});
 
 module.exports = app;
